bf794aa3bf32183040a3fd0105d2052de581ce49
rafiqaamini <rafiqaamini@gamil.com>
2018-09-07 14:45:13 +0700

    Initial Commit Dossen.java

---
+++ Dosen.java
@@ -0,0 +1,22 @@
+
+public class Dosen {
+	String nama;
+	String id;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String geId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
+
+
