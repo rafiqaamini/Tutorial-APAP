4cdd59045929ac2a443c97671f8769d7d376fbe0
rafiqaamini <rafiqaamini@gamil.com>
2018-09-07 14:38:52 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,27 @@
+
+public class Mahasiswa {
+	String nama;
+	String npm;
+	String kelas;
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNpm() {
+		return npm;
+	}
+	public void setNpm(String npm) {
+		this.npm = npm;
+	}
+	public String getKelas() {
+		return kelas;
+	}
+	public void setKelas(String kelas) {
+		this.kelas = kelas;
+	}
+	
+}
+
+
